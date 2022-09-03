b4e6c221ccc918f8e2392acf7257cb0e6e23cc6d
Satish Baradiya <baradiyasatish@gmail.com>
2022-09-03 11:48:07 +0530

    User Registration Added.

--- Portal/views.py
+++ Portal/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from Portal.forms import RegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/home')
+    else:
+        form = RegistrationForm()
+    
+    return render(request, "register.html",{
+        "form":form
+    }) 
+
+
+def home(requsest):
+    return HttpResponse("Welcome")
+
+def login(request):
+    return render(request, "login.html")
